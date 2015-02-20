f1857dce18f179b3002f6b69129daa0e7fe0ce4a
Karen Williams <mrs.brenchley@gmail.com>
2015-02-19 21:29:34 -0800

    Final version
    Just doing boys and girls. Cleaned up colors, filled in the FAQ. Best
    it's going to be at this point.

--- server.R
+++ server.R
@@ -9,48 +9,32 @@ m <- min(PearsonLee$child)
 shinyServer(
     function(input, output) {
         output$results <- renderPlot({
-
-            par.gen <- switch(input$pgen,
-                "Father" = "dad", 
-                "Mother" = "mom")
             
             chl.gen <- switch(input$cgen,
-                           "Son" = "son", 
-                           "Daughter" = "daughter")
+                           "Boy" = "son", 
+                           "Girl" = "daughter")
    
-            if (par.gen == "dad" && chl.gen == "son") {
-                fam = "fs"
+            if (chl.gen == "son") {
+                fam = "Son"
                 colors = brewer.pal(9, "Blues")
-                blabel = "Heights of Sons"
+                blabel = "Heights of Boys"
             }
 
-            if (par.gen == "dad" && chl.gen == "daughter") {
-                fam = "fd"
-                colors = brewer.pal(9, "Purples")
-                blabel = "Heights of Daughters"
-            }
-            
-            if (par.gen == "mom" && chl.gen == "son") {
-                fam = "ms"
-                colors = brewer.pal(9, "Reds")
-                blabel = "Heights of Sons"
-            }
-            
-            if (par.gen == "mom" && chl.gen == "daughter") {
-                fam = "md"
+            if (chl.gen == "daughter") {
+                fam = "Daughter"
                 colors = brewer.pal(9, "Greens")
-                blabel = "Heights of Daughters"
+                blabel = "Heights of Girls"
             }
             
             your_height <- (input$yft * 12) + input$yin
-                        
-            hist(PearsonLee$child[PearsonLee$gp == fam], col = colors,  
+                                               
+            hist(PearsonLee$child[PearsonLee$chl == fam], col = colors,  
                       main = blabel, breaks = 20,
-                      xlab = "")
-            abline(v = mean(PearsonLee$child[PearsonLee$gp == fam]), col = "orange", lwd = 3)
-            abline(v = your_height, col = "gold", lwd = 3)
+                      xlab = "height in inches")
+            abline(v = mean(PearsonLee$child[PearsonLee$chl == fam]), col = "orange", lwd = 3)
+            abline(v = your_height, col = "red", lwd = 3)
             legend("topright",legend = c("Average Height","Your Height"), 
-                   lty = "solid", col = c("orange", "gold"))
+                   lty = "solid", col = c("orange", "red"))
             })
     }
 )
--- ui.R
+++ ui.R
@@ -5,22 +5,20 @@ shinyUI(fluidPage (
     sidebarLayout(
 
         sidebarPanel(
-            helpText("In 1896 and 1903, Karl Pearson and Alice Lee gathered
-                     and recorded the heights of English families. That was 
-                     over 100 years ago."),
+            helpText("Ever wondered what life was like around the year 1900, in England? ",
+                     "We can get an idea of it, by taking a look at the heights of children who lived then.",
+                     "The histogram on the right shows you how many boys or girls (you choose) were at the different heights, in inches.",
+                     "Then you tell the app how tall you are, and you can compare your height with theirs."),
+
+            h3("Let's take a look at these children..."),
+            selectInput("cgen",
+                        label="Look at the heights of a",
+                        choices = c("Boy", "Girl"),
+                        selected = "Boy"),
             numericInput("yft", label="How tall are you? : feet", value = 5,
                          min = 4, max = 7, step = 1),
             numericInput("yin", label="How tall are you? : inches", value = 6,
-                         min = 0, max = 11, step = 1),
-            h3("Let's take a look at these children..."),
-            selectInput("pgen",
-                        label="The parent whose height was recorded was the",
-                        choices = c("Father", "Mother"),
-                        selected = "Father"),
-            selectInput("cgen",
-                        label="The child was a",
-                        choices = c("Son", "Daughter"),
-                        selected = "Son")
+                         min = 0, max = 11, step = 1)
                         ),
         mainPanel(
             tabsetPanel(type = "tabs",
@@ -28,21 +26,30 @@ shinyUI(fluidPage (
                                  
                         tabPanel("FAQ", tags$style("body {background-color: sepia;}"),
                                  h4("What is this app, and why should I care?"),
+                                 p("This app displays a histogram of the heights of boys and girls",
+                                   " collected in the years 1896 and 1903. The app also shows you",
+                                   " the average heights (with the orange bar) and your height (with the",
+                                   " red bar). This lets you compare how tall you are to how tall ",
+                                   "children were in these years in England. Are you taller or shorter",
+                                   "? Why could that be?"),
                                  br(),
                                  h4("OK, so how does it work?"),
-                                 br(),
-                                 h4("Who are the people in the picture?"),
-                                 p(
-                                     
-                                 ),
+                                 p("You pick whether to look at the boys or the girls, and the app ",
+                                   "instantly updates the display. At that point the app also instantly updates the",
+                                   " average. Whenever you change your height, the app also instantly updates",
+                                   " your height on the graph."),
                                  br(),
                                  h4("Anything else I should know?"),
-                                 p(
+                                 p("The dataset used by this app came from:"), br(),
+                                 p("Pearson, K. and Lee, A. (1896). Mathematical contributions to the theory of evolution. On telegony in man, etc. Proceedings of the Royal Society of London, 60 , 273-283."),
+                                 br(),
+                                 p(" and "), br(),
+                                 p("Pearson, K. and Lee, A. (1903). On the laws of inheritance in man: I. Inheritance of physical characters. Biometika, 2(4), 357-462. (Tables XXII, p. 415; XXV, p. 417; XXVIII, p. 419 and XXXI, p. 421.)"
                                  )
                                  
-                                 ) #tabPanel
+                                 ) #tabPanel 2
                         
-                        )
+                        ) #tabsetPanel
         )
     )
 ))
